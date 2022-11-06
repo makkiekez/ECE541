5fad6c6e96a30f214142e51e55eab62e3131ed20
makkiekez <117602558+makkiekez@users.noreply.github.com>
2022-11-06 13:39:06 -0800

    Update info-collector.py

--- info-collector.py
+++ info-collector.py
@@ -1,1 +1,13 @@
 
+print("Biodata form")
+
+name = input("Enter your name ")
+age = int(input("Enter your age "))
+dept = input("Enter your department ")
+
+def display(name, age, dept):
+    print("Name:", name)
+    print("Age:", age)
+    print("Department:", dept)
+
+display(name, age, dept)
